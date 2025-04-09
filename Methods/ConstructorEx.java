6a44fc924cc979b0919db616442d44036a945ba6
divakar-s-official <divaoffi01@gmail.com>
2025-04-09 12:12:54 +0530

    updated

---
+++ Methods/ConstructorEx.java
@@ -0,0 +1,20 @@
+package Methods;
+
+public class ConstructorEx {
+    /**
+     *
+     * => Constructor is a block of code similar to method
+     *
+     * usage:
+     *  =>  It is called when instance of the class is created
+     *  =>  At thr time of calling constructor , memory for the object is allocated in the memory
+     *  =>  Every time an object is created using new() keyword, at least one constructor is called .
+     *
+     *  2 types of constructors:
+     *  =>  Non parameterized constructor(no-arguments)
+     *  =>  parameterized constructor(with arguments)
+     */
+    public static void main(String[] args) {
+
+    }
+}
